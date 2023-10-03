a57b0362d6b5454d5b32d87cd233e8414f4b09e3
Cristian Barladeanu <cbarladeanu@griddynamics.com>
2023-10-03 15:57:25 +0300

    comitted code + task

---
+++ functions-vs-expressions/code-expressions.js
@@ -0,0 +1,35 @@
+const worldPopulation = 7900;
+
+function percentageOfWorld1 (population) { 
+let percentageResult = (population / worldPopulation) * 100;
+return percentageResult.toFixed(2) + '%';
+}
+
+const china = percentageOfWorld1(1441);
+const germany = percentageOfWorld1(40);
+const france  = percentageOfWorld1(50);
+
+console.log(china);
+console.log(germany);
+console.log(france);
+
+const percentageOfWorld2 = function (population) { 
+    let percentageResult = (population / worldPopulation) * 100;
+    return percentageResult.toFixed(2) + '%';
+}
+
+const romania = percentageOfWorld2(10);
+const mexico = percentageOfWorld2(80);
+const uk = percentageOfWorld2(45);
+
+console.log(romania);
+console.log(mexico);
+console.log(uk);
+
+
+
+
+
+
+
+
